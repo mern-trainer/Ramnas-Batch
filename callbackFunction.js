ba3aaa0a7eaa0c52795a748616f72e82ff49d441
VISHNU M K <futuralabs.mern@gmail.com>
2025-01-08 11:00:28 +0530

    updated: callback function

--- callbackFunction.js
+++ callbackFunction.js
@@ -34,24 +34,64 @@
 
 // callback function => function passed as an argument to another function
 
-function display(value) {
-    console.log(value)
-}
+// function display(value) {
+//     console.log(value)
+// }
 
-function sum(num1, num2, callbackFunc) {
-    const r = num1 + num2
-    callbackFunc(r)
-}
+// function sum(num1, num2, callbackFunc) {
+//     const r = num1 + num2
+//     callbackFunc(r)
+// }
 
-sum(10, 50, display)
+// sum(10, 50, display)
 
 // Higher order function (sum) => function that takes another function as an argument
 // first order function (display) => function passed as an argument to another function
 
+// function sum(a, b) { // parameter
+//     return a + b
+// }
+
+// arrow function => shorthand for writing a function
+
+const sum = (a, b) => a + b
+
+sum(10, 20) // arguments
+
+// sum of array elements
+
+// setTimeout => execute a function after a specified amount of time
+// setInterval => execute a function at regular intervals
+
+// async, 
+
+const sumOfArray = (arr, callbackFunc) => {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    setTimeout(() => {
+        callbackFunc(sum)
+    }, 3000);
+}
 
 
+const arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+sumOfArray(arr, function (value) {
+    console.log(value);
+})
 
 
+// const timerEvement = document.getElementById('timer')
 
+// setInterval(() => {
+//     const date = new Date()
+//     timerEvement.innerHTML = date
+// }, 1000);
+
+// setTimeout(() => {
+//     console.log('hello');
+// }, 5000);
 
 

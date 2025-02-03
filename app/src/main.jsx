7d19f8149d23292c38d79339ff63f5067aa14e18
VISHNU M K <futuralabs.mern@gmail.com>
2025-02-03 10:52:05 +0530

    react bootstrap

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client" // named Import
 import App from "./App"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 
 createRoot(document.getElementById('root')).render(<App />)

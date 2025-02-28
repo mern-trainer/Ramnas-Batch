596572cc027491598401bcb129a69af3c4f13a88
VISHNU M K <futuralabs.mern@gmail.com>
2025-02-28 10:02:50 +0530

    useEffect: Unmounting

--- app/src/Pages/EffectPage.jsx
+++ app/src/Pages/EffectPage.jsx
@@ -5,7 +5,7 @@
 // 3 stages of life cycle of a component -> mounting, updating, unmounting
 // mounting -> called after component is rendered for the first time
 // updating -> called when component is updated
-// unmounting -> called when component is removed
+// unmounting -> called before component is removed
 
 import { useEffect, useState } from "react"
 
@@ -16,7 +16,10 @@ const EffectPage = () => {
 
     useEffect(() => {
         console.log(counter);
-    }, [counter]) // dependency array -> updating stage
+        return () => { // cleanup -> unmounting
+            console.log("Cleanup");
+        }
+    }, [counter])
 
     return <div className="d-flex align-items-center gap-3 justify-content-center vh-100">
         <div className="d-flex flex-column gap-3">

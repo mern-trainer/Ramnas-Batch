1c51ae7aa0d3f4562c1c89451161d19fc78f6931
VISHNU M K <futuralabs.mern@gmail.com>
2025-03-28 10:49:42 +0530

    nodejs started

---
+++ MERN/Server/app.js
@@ -0,0 +1,27 @@
+// import readline from "readline-sync" 
+const readline = require("readline-sync")
+
+const num1 = readline.questionInt("Enter Number 1:")
+const num2 = readline.questionInt("Enter Number 2:")
+
+console.log(`The sum is ${num1 + num2}`)
+
+// NodeJS -> Nodejs is a runtime environment for executing JavaScript
+// code outside of a web browser
+
+// npm -> Node Package Manager
+
+// local Modules or core Modules or Built-in Modules, Third Party Modules
+
+// core Modules -> Nodejs comes with core modules -> installation is not required
+
+// examples of core modules -> fs, path, os, http, url, events, crypto, dns
+
+// fs -> file system -> file system is a module that provides a way of working with the file system
+// path -> path module -> path is a module that provides a way of working with file and directory paths
+// os -> operating system -> operating system is a module that provides a way of working with the operating system
+// http -> http module -> http is a module that provides a way of working with the http protocol
+// url -> url module -> url is a module that provides a way of working with the url protocol
+// events -> events module -> events is a module that provides a way of working with events
+// crypto -> crypto module -> crypto is used for encrypting and decrypting data
+// dns -> dns module -> dns is a module that provides a way of working with the domain name system

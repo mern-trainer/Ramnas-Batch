dba4cceb2920bb501e0eca731036212867ae8dc3
VISHNU M K <futuralabs.mern@gmail.com>
2025-04-02 10:12:04 +0530

    fs module

--- MERN/Server/app.js
+++ MERN/Server/app.js
@@ -39,12 +39,44 @@
 // console.log(os.userInfo());
 // console.log(os.type());
 
-const dns = require("dns")
+// const dns = require("dns")
 
-// dns -> Domain name system -> dns is a module that provides a way of working with the domain name system
+// // dns -> Domain name system -> dns is a module that provides a way of working with the domain name system
 
-dns.lookup("www.google.com", (err, address, family) => {
-    console.log(address);
-    console.log(family);
-    console.log(err);
+// dns.lookup("www.google.com", (err, address, family) => {
+//     console.log(address);
+//     console.log(family);
+//     console.log(err);
+// })
+
+// fs -> file system -> file system is a module that provides a way of working with the file system.
+
+const fs = require("fs")
+
+// create and write a file
+fs.writeFile("fs_sample.txt", "Hello World", () => {
+    console.log("File created");
+})
+
+// read a file
+fs.readFile("fs_sample.txt", { encoding: "utf-8" }, (err, text) => {
+    if (err) {
+        return console.log(err.message);
+    }
+    console.log(text);
+})
+
+// Modify a file
+fs.appendFile("fs_sample.txt", " Hey there", () => {
+    console.log("File modified");
+})
+
+// Delete a file
+// fs.unlink("fs_sample.txt", () => {
+//     console.log("File deleted");
+// })
+
+// Rename a file
+fs.rename("fs_sample.txt", "fs.txt", () => {
+    console.log("File renamed");
 })

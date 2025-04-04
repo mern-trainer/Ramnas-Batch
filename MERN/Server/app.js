00e3e1c98ff2aeb8a20f43adf3ff30b44cb8e5c4
VISHNU M K <futuralabs.mern@gmail.com>
2025-04-04 10:58:19 +0530

    todo list

--- MERN/Server/app.js
+++ MERN/Server/app.js
@@ -2,6 +2,7 @@
 
 const http = require("http")
 const url = require("url")
+const { v4: uuidv4 } = require("uuid")
 
 const server = http.createServer()
 
@@ -16,13 +17,80 @@ const server = http.createServer()
 // 409 -> Conflict
 // 500 -> Internal server error
 
+let todoList = []
+
 server.on("request", (request, response) => {
     
     const method = request.method
-    const { pathname, query } = url.parse(request.url, true)
-    const { hi, hello } = query
-    response.end(hi + " " + hello)
-    
+    const { pathname: path_name, query } = url.parse(request.url, true)
+
+    if (path_name == "/api/todo" && method == "POST") {
+        const { title, description } = query
+        if (!title || !description) {
+            response.writeHead(400, { "content-type": "application/json" })
+            return response.end(JSON.stringify({ error: "Title and Description are required." }))
+        }
+        const dateTime = new Date().toISOString()
+        const taskObj = {
+            id: uuidv4(),
+            title,
+            description,
+            status: "Pending",
+            createdAt: dateTime,
+            updatedAt: dateTime
+        }
+        todoList.push(taskObj)
+        response.writeHead(201, { "content-type": "application/json" })
+        return response.end(JSON.stringify({ message: "Task created", task: taskObj }))
+    }
+
+    if (path_name == "/api/todo" && method == "GET") {
+        response.writeHead(200, { "content-type": "application/json" })
+        return response.end(JSON.stringify({ message: "Task fetched", tasks: todoList }))
+    }
+
+    if (path_name == "/api/todo" && method == "DELETE") {
+        const { id } = query
+        if (!id) {
+            response.writeHead(400, { "content-type": "application/json" })
+            return response.end(JSON.stringify({ error: "Id is required" }))
+        }
+        const index = todoList.findIndex(todo => todo.id == id)
+        if (index == -1) {
+            response.writeHead(404, { "content-type": "application/json" })
+            return response.end(JSON.stringify({ error: "Task does not exist" }))
+        }
+        const res = todoList.splice(index, 1)
+        response.writeHead(200, { "content-type": "application/json" })
+        return response.end(JSON.stringify({ message: "task deleted", deleted: res }))
+    }
+
+    if (path_name == "/api/todo" && method == "PATCH") {
+        const { id, title, description, status } = query
+        if (!id) {
+            response.writeHead(400, { "content-type": "application/json" })
+            return response.end(JSON.stringify({ error: "Id is required" }))
+        }
+        const obj = {}
+        if (title) obj.title = title
+        if (description) obj.description = description
+        if (status) obj.status = status
+        if (title || description || status) {
+            obj.updatedAt = new Date().toISOString()
+        }
+        if(!title && !description && !status) {
+            response.writeHead(400, { "content-type": "application/json" })
+            return response.end(JSON.stringify({ error: "Title, Description or Status required." }))
+        }
+        const index = todoList.findIndex(todo => todo.id == id)
+        if (index == -1) {
+            response.writeHead(404, { "content-type": "application/json" })
+            return response.end(JSON.stringify({ error: "Task does not exist" }))
+        }
+        todoList[index] = { ...todoList[index], ...obj }
+        response.writeHead(200, { "content-type": "application/json" })
+        return response.end(JSON.stringify({ message: "task updated", updated: todoList[index] }))
+    }
 })
 
 server.listen(8080, () => {
